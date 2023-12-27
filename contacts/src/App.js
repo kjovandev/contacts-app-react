ada23e8a006c395e20fef5377276a40e9e1778cb
jovan <krsticjovan@protonmail.com>
2023-12-27 12:24:14 +0100

    mapping components try 1

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from "./cards"
+import Contacts from "./components"
+
+
+// failed to implement the map function
+
+// function createCard(contact){
+//   return (
+//     <Card 
+//     key={contact.id}
+//     name={contact.name}
+//     img={contact.imgUrl}
+//     tel={contact.phoneNumber}  
+//     email={contact.email}
+//   />
+//   );
+// }
 
 function App() {
   return (
@@ -10,26 +26,31 @@ function App() {
         Contacts
       </header>
 
+      {/* failed to implement the math function  will get back to it later*/}
+      {/* {Contacts.map(createCard)} */}
+
+
+
+
       <Card
-          name="Person 1"
-          email="person@random.com"
-          phoneNumber="+1 88 785554523"
-          imgSource="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww"
-        />
-        <Card
-          name="Person 2"
-          email="another@person.com"
-          phoneNumber="+1 88 123456789"
-          imgSource="https://miro.medium.com/v2/resize:fit:1400/1*y_uyQN1xEjppGVWJJkibMQ.jpeg"
-        />
-        <Card
-          name="Person 3"
-          email="yetanother@person.com"
-          phoneNumber="+1 88 987654321"
-          imgSource="https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww"
-        />
-      </div>
-    
+        name={Contacts[0].name}
+        email={Contacts[0].email}
+        phoneNumber={Contacts[0].phoneNumber}
+        imgSource={Contacts[0].imgUrl}
+      />
+      <Card
+        name={Contacts[1].name}
+        email={Contacts[1].email}
+        phoneNumber={Contacts[1].phoneNumber}
+        imgSource={Contacts[1].imgUrl}
+      />
+      <Card
+        name={Contacts[2].name}
+        email={Contacts[2].email}
+        phoneNumber={Contacts[2].phoneNumber}
+        imgSource={Contacts[2].imgUrl}
+      />
+    </div>
   );
 }
 
---
+++ contacts/src/components.js
@@ -0,0 +1,25 @@
+const Contacts = [
+    {
+        id:1,
+        name: "Bourne",
+        email: "bourne@gmail.com",
+        imgUrl: "https://m.media-amazon.com/images/M/MV5BMTM0NzYzNDgxMl5BMl5BanBnXkFtZTcwMDg2MTMyMw@@._V1_FMjpg_UX1000_.jpg",
+        phoneNumber: "+1 111 222 33"
+    },
+    {
+        id:2,
+        name: "Bond",
+        email: "bond@gmail.com",
+        imgUrl: "https://d2w9m16hs9jc37.cloudfront.net/dimg/blog/2023/07/hockerty_sean_connery_james_bond_8c9de868_2912_4a4e_a539_2ecda28d3ab4.jpg",
+        phoneNumber: "+1 111 888 33"
+    },
+    {
+        id:3,
+        name: "Bourne",
+        email: "bourne@gmail.com",
+        imgUrl: "https://m.media-amazon.com/images/M/MV5BMTM0NzYzNDgxMl5BMl5BanBnXkFtZTcwMDg2MTMyMw@@._V1_FMjpg_UX1000_.jpg",
+        phoneNumber: "+1 111 255 33"
+    }
+];
+
+export default Contacts;
